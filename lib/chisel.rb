7814a045e8ac7fae6bed624b3dc796c8a4251bd9
Ashish Vaid <vaidashi@Ashishs-MacBook-Air.local>
2017-05-31 13:47:35 -0600

    created first test and writing pseudocode

---
+++ lib/chisel.rb
@@ -0,0 +1,18 @@
+
+class Chisel
+
+  ## take in markdown input
+
+  markdown_file = File.read(my_input.markdown)
+
+
+
+  ## convert to html
+  ## write to html
+
+  def converts_to_markdown
+    
+  end
+
+
+end
